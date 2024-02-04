87ad55bedc6f4d99f67a6b0935320e84f222c069
rahunak <euhene@linnovate.net>
2024-02-04 06:24:13 +0300

    #refactor: navigation: goUp, goDown

--- nav_and_work/navigation.js
+++ nav_and_work/navigation.js
@@ -1,25 +1,36 @@
 import path from 'path';
 import os from 'os';
 
-function changeDirectory(destination) {
-  if (destination === undefined) {
-    try {
-      if (process.cwd() === os.homedir()) {
-        return;
-      }
-      process.chdir(path.resolve(process.cwd(), '..'));
+/**
+ * Move the current working directory up one level,
+ * but unless the current directory is already the user's home directory.
+ *
+ * @return {undefined}
+ */
+function goUp() {
+  try {
+    if (process.cwd() === os.homedir()) {
+      return;
     }
-    catch (err) {
-      console.error('Operation failed');
-    }
+    process.chdir(path.resolve(process.cwd(), '..'));
   }
-  else {
-    try {
-      process.chdir(path.resolve(process.cwd(), destination));
-    }
-    catch (err) {
-      console.error('Operation failed');
-    }
+  catch (err) {
+    console.error('Operation failed');
   }
 }
-export default changeDirectory;
+
+/**
+ * Change the current working directory to the specified path, catching any errors that occur.
+ *
+ * @param {string} pathDestination - the destination path to change to
+ * @return {void}
+ */
+function goDown(pathDestination) {
+  try {
+    process.chdir(path.resolve(process.cwd(), pathDestination));
+  }
+  catch (err) {
+    console.error('Operation failed');
+  }
+}
+export { goUp, goDown };
